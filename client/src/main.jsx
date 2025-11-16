f0208f3e4109c23b7429e6e9bf54fc69c600aead
shaw2024 <167036376+shaw2024@users.noreply.github.com>
2025-11-16 04:54:12 +0000

    Switch to HashRouter for better GitHub Pages compatibility

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./App";
 import store from "./store";
@@ -22,9 +22,9 @@ async function bootstrap() {
   createRoot(document.getElementById("root")).render(
     <React.StrictMode>
       <Provider store={store}>
-        <BrowserRouter basename={import.meta.env.PROD ? "/greensocialsite" : "/"}>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </Provider>
     </React.StrictMode>
   );
